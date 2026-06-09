d2c6447c2132db88854fb3f77df48aac5d6cb0ef
Piotr Korniak <piotrkorniak99@gmail.com>
2026-06-09 18:38:24 +0200

    Enable auto-migrate on all environments

--- Api/Extensions/WebApplicationExtensions.cs
+++ Api/Extensions/WebApplicationExtensions.cs
@@ -9,12 +9,9 @@ public static class WebApplicationExtensions
 {
     public static async Task<WebApplication> ConfigurePipelineAsync(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
-        {
-            using var scope = app.Services.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            await db.Database.MigrateAsync();
-        }
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
 
         app.UseExceptionHandler();
         app.UseCors();
